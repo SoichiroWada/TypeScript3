29c7e9175689c18773c790fc04aaebff1692917d
SoichiroWada <soichirowada@ipmulticast.net>
2023-09-10 02:08:34 +0900

    changes OK

---
+++ public/sandbox.js
@@ -0,0 +1,10 @@
+"use strict";
+let ninja = {
+    name: 'mario',
+    belt: 'black coffee',
+    age: 98
+};
+console.log(ninja);
+const source = { eyecolor: 'blue' };
+Object.assign(ninja, source);
+console.log(ninja);
---
+++ src/sandbox.ts
@@ -0,0 +1,13 @@
+let ninja = {
+  name: 'mario',
+  belt: 'black coffee',
+  age: 98
+}
+
+console.log(ninja)
+
+const source = { eyecolor:'blue' }
+
+Object.assign(ninja, source);
+
+console.log(ninja)
